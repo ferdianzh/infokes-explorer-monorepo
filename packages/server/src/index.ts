60d230abef18d70bf752e042490a1444ad9c3d84
ferdianzh <ferdianzh30@gmail.com>
2026-03-07 02:51:24 +0700

    first commit

---
+++ packages/server/src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3001);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
+// test change
