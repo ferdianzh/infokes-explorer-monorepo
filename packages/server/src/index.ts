763b46126156278702a7e97e12af50740fe43d3a
ferdianzh <ferdianzh30@gmail.com>
2026-03-07 21:32:24 +0700

    server: add & setup cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import { itemsController } from "./api/items/items.controller";
+import cors from "@elysiajs/cors";
 
 export const app = new Elysia()
   .get("/", () => "Hello Elysia")
+  .use(cors())
   .use(itemsController);
 
 if (import.meta.main) {
